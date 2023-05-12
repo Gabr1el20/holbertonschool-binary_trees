026d37ce15b4e44ebb882117b4dcd76623e2cbb5
Gabr1el20 <6327@holbertonstudents.com>
2023-05-12 12:23:57 -0700

    Fixed segmentation faul when parent == NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,18 +7,19 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
-    if (newNode)
-    {
-        newNode->n = value;
-        newNode->parent = parent;
-        newNode->left = NULL;
-        newNode->right = NULL;
-    }
-    else
-    {
-        free(newNode);
-        return (NULL);
-    }
-    return (newNode);
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode)
+	{
+		newNode->n = value;
+		newNode->parent = parent;
+		newNode->left = NULL;
+		newNode->right = NULL;
+	}
+	else
+	{
+		free(newNode);
+		return (NULL);
+	}
+	return (newNode);
 }
