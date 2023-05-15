041f7aa17fbd4bf9d87040fd05040d638a3d1f44
Gabr1el20 <6327@holbertonstudents.com>
2023-05-15 11:25:36 -0700

    Added 'sibling' function and handled exact condition

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,9 +11,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (!node->parent->left || !node->parent->right)
 		return (NULL);
-	if (node->parent->right)
+	if (node->parent->right && node->parent->right != node)
 		return (node->parent->right);
-	if (node->parent->left)
+	if (node->parent->left && node->parent->left != node)
 		return (node->parent->left);
 	return (NULL);
 }
