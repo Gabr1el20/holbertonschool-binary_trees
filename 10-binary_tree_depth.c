400dc44d78dd5fb454f118396cee85523b5a0a3b
Gabr1el20 <6327@holbertonstudents.com>
2023-05-15 09:38:59 -0700

    Handled betty in depth function

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,15 +7,15 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t count = 0;
-    
-    if (!tree)
-        return (0);
+	size_t count = 0;
 
-    while (tree->parent)
-    {
-        count++;
-        tree = tree->parent;
-    }
-    return (count);
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		count++;
+		tree = tree->parent;
+	}
+	return (count);
 }
