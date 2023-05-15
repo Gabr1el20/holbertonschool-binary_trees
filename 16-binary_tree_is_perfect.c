ba8175269ca837cf48129c91a1975f8cc6da5a7e
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-15 13:15:06 -0700

    Perfect Check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -85,6 +85,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+
 	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
 	{
 		if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0)
